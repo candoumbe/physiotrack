kalic-io/agenda

WIP : Adds tye support

src/services/agenda/Agenda.API/ServiceCollectionExtensions.cs
@@ -163,7 +163,7 @@ namespace Agenda.API
                 {
                     IConfiguration configuration = serviceProvider.GetRequiredService<IConfiguration>();
                     builder.UseNpgsql(
-                        configuration.GetConnectionString("Agenda"),
+                        configuration.GetConnectionString("agenda-db"),
                         options => options.EnableRetryOnFailure(5)
                                           .MigrationsAssembly(typeof(AgendaContext).Assembly.FullName)
                     );

candoumbe/physiotrack

Measures.API/ServiceCollectionExtensions.cs
@@ -161,7 +161,7 @@ namespace Measures.API
                 {
                     IConfiguration configuration = serviceProvider.GetRequiredService<IConfiguration>();
                     builder.UseNpgsql(
-                        configuration.GetConnectionString("Measures"),
+                        configuration.GetConnectionString("measures-db"),
                         options => options.EnableRetryOnFailure(5)
                             .MigrationsAssembly(typeof(MeasuresContext).Assembly.FullName)
                     );