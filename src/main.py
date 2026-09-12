d6414aba162673f4905ddf5d0bef7b57a485f323
Cyrille NDOUMBE <candoumbe@users.noreply.github.com>
2026-09-12 11:34:14 +0000

    feat(api): add execution entrypoint in main.py

--- src/main.py
+++ src/main.py
@@ -5,3 +5,7 @@ from features.health.router import router as health_router
 app = FastAPI(title="PhysioTrack API")
 
 app.include_router(health_router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0")
